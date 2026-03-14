0b7b9a19ecd83a143558243591890854fe2c4560
Codingnaveen46 <naveenreddy4411@gmail.com>
2026-03-14 22:36:10 +0530

    learnt basics of python

---
+++ Day1/Day1.py
@@ -0,0 +1,21 @@
+# Asking user for their namne 
+name = input("Enter your name:")
+
+# Printing the name after asking 
+print("Hello",name)
+
+#pseudocode writing the rough ideas or structure how it works before doing actual code 
+
+#difference between parameters and functions
+
+# Parameter = placeholder variable in function definition
+# Argument = actual value passed when calling the function
+
+def add (x,y):
+    return x +y
+a = int(input("Enter the number: "))
+b = int(input("Enter second number: "))
+result = add(a,b)
+
+print("Result: ",result)
+
