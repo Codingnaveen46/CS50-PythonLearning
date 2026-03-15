70e7859e90db9c95f81fcebdba78963249bae5b2
Codingnaveen46 <naveenreddy4411@gmail.com>
2026-03-15 20:07:23 +0530

    learnt about the stringd

---
+++ python/Day2.py
@@ -0,0 +1,22 @@
+
+strings in python 
+
+name = input("Enter your name :").strip().title()
+
+#Remove the whitespace from the str and capatilize the letter 
+
+#name = name.strip().title()
+
+first, last = name.split(" ")
+
+# capatalizr user's name 
+
+print(f"helllo, {name}")
+
+
+
+#float 
+
+
+
+
---
+++ python/calculator.py
@@ -0,0 +1,15 @@
+# num1 = int (input("Enter the number: "))
+# num2 = int (input("Enter the number: "))
+
+
+# print(num1 + num2)
+
+
+num3 = float (input("Enter the number: "))
+num4 = float (input("Enter the number: "))
+
+z = (num3/num4 )
+
+print(f"{z:.2f}")
+
+# print(z)
